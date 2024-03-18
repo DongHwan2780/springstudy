6e9c9c4350a26453ccfef6fbe8b2b2ef3db5cd02
DongHwan2780 <donghwan102@naver.com>
2024-03-18 17:51:30 +0900

    240318

---
+++ 05_junit_logback/src/main/webapp/resources/sql/contact_t.sql
@@ -0,0 +1,16 @@
+DROP TABLE CONTACT_T;
+CREATE TABLE CONTACT_T (
+  CONTACT_NO NUMBER             NOT NULL,
+  NAME       VARCHAR2(100 BYTE) NOT NULL,
+  MOBILE     VARCHAR2(20 BYTE),
+  EMAIL      VARCHAR2(100 BYTE),
+  ADDRESS    VARCHAR2(200 BYTE),
+  CREATED_AT VARCHAR2(20 BYTE),
+  CONSTRAINT PK_CONTACT PRIMARY KEY(CONTACT_NO)
+);
+
+DROP SEQUENCE CONTACT_SEQ;
+CREATE SEQUENCE CONTACT_SEQ NOCACHE;
+
+insert into contact_t values(contact_seq.nextval, 'name', 'mobile', 'email', 'address', to_char(current_date, 'yyyy-mm-dd hh24:mi:ss'));
+commit;
